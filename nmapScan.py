31b426702d3178de411b7819798dac46718f73c1
wd <d1314ziting@163.com>
2018-08-28 10:32:45 +0800

    Can't run bug

--- nmapScan.py
+++ nmapScan.py
@@ -11,16 +11,21 @@ import glob
 from multiprocessing import   Process,Pool
 import nmap
 import os
-
+import re
 def getAll(filepath):
     file_lst=glob.glob1(filepath,'*.txt')
     ip_lst=[]
 
     for filename in file_lst:
-        print '\033[1;31m %s \033[0m' % filename
+        print ' %s ' % filename
         with open(filename,'r') as f:
             for ip in f.readlines():
                 ip=ip.strip()
+
+                ip=re.findall('\d+\.\d+\.\d+\.\d+',ip)
+                if ip :
+                    # print ip
+                    ip=ip[0]
                 try:
                     ip_=IP(ip)
                     for ip in ip_:
@@ -28,26 +33,31 @@ def getAll(filepath):
                         ip_lst.append(ip)
                 except:
                     with open('failed.log','a+') as f:
+                        print '[!] Failed : %s ,record data to failed.log!' % ip
                         f.write(ip+'\n')
     return  ip_lst
 
 
 
 # -sV -p- -T4 -Pn -n --min-parallelism 512 --min-hostgroup 64
-def portScan(ip,command='-sV -p- -T4 -Pn -n  '):
+def portScan(ip,reportpath,command='-sV -p- -T4 -Pn -n '):
+
     ip=str(ip)
-    filenanme=REPORTPAHT+'/'+ip+'.xml'
+
+    filenanme=reportpath+'/'+ip+'.xml'
+
     if os.path.exists(filenanme):
         print 'exists'
         return
-
+    print '[-] Scanning : %s ....' % ip
     nm=nmap.PortScanner()
     nm.scan(ip,arguments=command)
+
     with open(filenanme,'w') as f:
         f.write(nm.get_nmap_last_output())
     print nm.command_line()
 
-# portScan('192.168.1.1')
+
 
 # def reportExcel(datalst,title=TI):
 
@@ -57,23 +67,28 @@ if __name__ == '__main__':
     if len(sys.argv)<3:
         print '[!] Usage: nmapScan.py ipfiledir [reportxmldir]'
         print '[!] Demo: nmapScan.py  ip_path  RESULT_XML_DIR'
+        exit(1)
     else:
 
-        REPORTPAHT=sys.argv[2]
-        REPORTPAHT= 'report' if not REPORTPAHT else REPORTPAHT
+        REPORTPATH=sys.argv[2]
+        REPORTPATH= 'report' if not REPORTPATH else REPORTPATH
         IPPATH=sys.argv[1]
+        print REPORTPATH,IPPATH
         if not os.path.exists(IPPATH):
             print "[!] '%s' path does not exists!" % IPPATH
             exit(1)
-    if not os.path.exists(REPORTPAHT):
-        os.mkdir(REPORTPAHT)
-    p_lst=[]
+    if not os.path.exists(REPORTPATH):
+        os.mkdir(REPORTPATH)
+    result=[]
     pool= Pool()
-    for ip in getAll(IPPATH):
-        pool.apply_async(portScan,(ip,))
+    ip_lst=getAll(IPPATH)
+    print "[*] Total: %s" % len(ip_lst)
+    for ip in ip_lst:
+
+        # result.append( pool.apply_async(portScan,(ip,)))
+        pool.apply_async(portScan,(ip,REPORTPATH))
 
     pool.close()
     pool.join()
-
-    print "complete！"
+    print "complete!"
     pass
